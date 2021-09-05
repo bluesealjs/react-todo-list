e919cf38bfac5c795364935b80fb0e301cd89e95
sudhir pradhan <rcmkkskp@gmail.com>
2021-09-05 12:38:17 +0530

    test 01

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
-import React, { Component } from "react";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
+import React, { Component } from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import uuid from "uuid";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 
 class App extends Component {
   state = {
     items: [],
     id: uuid(),
-    item: "",
-    editItem: false
+    item: '',
+    editItem: false,
   };
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({
-      item: e.target.value
+      item: e.target.value,
     });
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
 
     const newItem = {
       id: this.state.id,
-      title: this.state.item
+      title: this.state.item,
     };
 
     const updatedItems = [...this.state.items, newItem];
 
     this.setState({
       items: updatedItems,
-      item: "",
+      item: '',
       id: uuid(),
-      editItem: false
+      editItem: false,
     });
   };
 
   clearList = () => {
     this.setState({
-      items: []
+      items: [],
     });
   };
 
-  handleDelete = id => {
-    const filteredItems = this.state.items.filter(item => item.id !== id);
+  handleDelete = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
 
     this.setState({
-      items: filteredItems
+      items: filteredItems,
     });
   };
 
-  handleEdit = id => {
-    const filteredItems = this.state.items.filter(item => item.id !== id);
+  handleEdit = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
 
-    const selectedItem = this.state.items.find(item => item.id === id);
+    const selectedItem = this.state.items.find((item) => item.id === id);
     console.log(selectedItem);
 
     this.setState({
       items: filteredItems,
       item: selectedItem.title,
       editItem: true,
-      id: id
+      id: id,
     });
   };
 
@@ -69,7 +69,9 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
-            <h3 className="text-capitalize text-center">todo input</h3>
+            <h3 className="text-capitalize text-center">
+              todo input - PWA Test
+            </h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
