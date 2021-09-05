4f918bd18fde71df8984337b28872dadd0c2a1a7
sudhir pradhan <rcmkkskp@gmail.com>
2021-09-05 19:43:19 +0530

    test 03

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CacheBuster from './components/CacheBuster';
 
 const render = (Component) => {
   return ReactDOM.render(<Component />, document.getElementById('root'));
@@ -12,6 +13,19 @@ const render = (Component) => {
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
+
+// for invalidating cache and force reload
+const MyApp = (
+  <CacheBuster>
+    {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+      return (
+        <>
+          <App />
+        </>
+      );
+    }}
+  </CacheBuster>
+);
 
 render(App);
 
